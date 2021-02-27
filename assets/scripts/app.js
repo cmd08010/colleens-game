e0eb81136f6c6fb1196ac4c127b5a9c63cda2d6a
cmd08010 <colleendunion@gmail.com>
2021-02-27 18:29:53 -0500

    Fix errors from bad merge

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,29 +1,41 @@
+
 'use strict'
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const userEvents = require('./user/events')
 const gameEvents = require('./game/events')
-const store = require('./store')
+
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
-
   // user event handlers
   $('#user-signed-in').hide()
+  $('#user-options').hide()
+  $('#signin-btn').on('click', userEvents.onShowSigninButton)
+  $('#signup-btn').on('click', userEvents.onShowSignupButton)
+  $('#sign-in').hide()
+  $('#sign-up').hide()
   $('#sign-up').on('submit', userEvents.onSignUp)
   $('#sign-in').on('submit', userEvents.onSignIn)
+  $('#change-password-button').on('click', userEvents.onShowChangePassword)
   $('#change-password').on('submit', userEvents.onChangePassword)
   $('#sign-out').on('click', userEvents.onSignOut)
 
   // game event handlers
+  $('#game-board').hide()
   $('#create-game').on('click', gameEvents.onCreateGame)
-  $('#game-board').on('click', gameEvents.onUpdateGame)
-//  $('#game-board').on('click', gameEvents.onTurnChange)
-// remove once completed - this shows my stored stuff
 
-$('#show-game').on('click', gameEvents.onShowGames)
-  $('#storage').on('click', () => console.log(store))
+  $('.box').on('click', gameEvents.onUpdateGame)
+  //  $('#game-board').on('click', gameEvents.onTurnChange)
+
+  $('#hide-game').hide()
+  $('#show-game').on('click', gameEvents.onShowGames)
+
+  $('#hide-game').on('click', gameEvents.onHideGames)
+
+  // remove once completed - this shows my stored stuff
+//  $('#storage').on('click', () => console.log(store))
 })
